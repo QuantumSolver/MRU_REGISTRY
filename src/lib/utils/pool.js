67e491a8bd9821db279168d7a08c619e947c77d7
Marcques Mouton <marcquesmouton1992@gmail.com>
2022-07-27 01:39:49 +0400

    update/generate-accession

--- src/lib/utils/pool.js
+++ src/lib/utils/pool.js
@@ -2,11 +2,11 @@ import pkg from 'pg';
 const { Pool } = pkg;
 
 const pool = new Pool({
-  "host": `${process.env.DB_HOST}`,
-  "port": process.env.DB_PORT,
-  "user": `${process.env.DB_USER}`,
-  "password" : `${process.env.DB_PASS}`,
-  "database" : `${process.env.DB_NAME}`,
+  "host": "132.145.56.181",
+  "port": 5432,
+  "user":"clinlims",
+  "password" : "bnjWYLBdTMrx",
+  "database" : "clinlims",
   "max": 20,
   "connectionTimeoutMillis" : 0,
   "idleTimeoutMillis": 30000
